ed25d5b6df53e448cddbe0fccd03d9098fe6b179
jaycee <vsquare396@gmail.com>
2019-05-14 11:44:42 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:todo/todo.dart';
+
+import 'new_todo.dart';
+
+void main() => runApp(Main());
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterTodo',
+      home: Home(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  List<Todo> items = new List<Todo>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Flutter Todo',
+            key: Key('main-app-title'),
+          ),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => goToNewItemView(),
+        ),
+        body: renderBody());
+  }
+
+  Widget renderBody() {
+    if (items.length > 0) {
+      return buildListView();
+    } else {
+      return emptyList();
+    }
+  }
+
+  Widget emptyList() {
+    return Center(child: Text('No items'));
+  }
+
+  Widget buildListView() {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) {
+        return buildItem(items[index], index);
+      },
+    );
+  }
+
+  Widget buildItem(Todo item, index) {
+    return Dismissible(
+      key: Key('${item.hashCode}'),
+      background: Container(color: Colors.red[700]),
+      onDismissed: (direction) => _removeItemFromList(item),
+      direction: DismissDirection.startToEnd,
+      child: buildListTile(item, index),
+    );
+  }
+
+  Widget buildListTile(item, index) {
+    return ListTile(
+      onTap: () => changeItemCompleteness(item),
+      onLongPress: () => goToEditItemView(item),
+      title: Text(
+        item.title,
+        key: Key('item-$index'),
+        style: TextStyle(
+            color: item.completed ? Colors.grey : Colors.black,
+            decoration: item.completed ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Icon(
+        item.completed ? Icons.check_box : Icons.check_box_outline_blank,
+        key: Key('completed-icon-$index'),
+      ),
+    );
+  }
+
+  void changeItemCompleteness(Todo item) {
+    setState(() {
+      item.completed = !item.completed;
+    });
+  }
+
+  void goToNewItemView() {
+    // Here we are pushing the new view into the Navigator stack. By using a
+    // MaterialPageRoute we get standard behaviour of a Material app, which will
+    // show a back button automatically for each platform on the left top corner
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return NewTodoView();
+    })).then((title) {
+      if (title != null) {
+        addItem(Todo(title: title));
+      }
+    });
+  }
+
+  void addItem(Todo item) {
+    // Insert an item into the top of our list, on index zero
+    items.insert(0, item);
+  }
+
+  void goToEditItemView(item) {
+    // We re-use the NewTodoView and push it to the Navigator stack just like
+    // before, but now we send the title of the item on the class constructor
+    // and expect a new title to be returned so that we can edit the item
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return NewTodoView(item: item);
+    })).then((title) {
+      if (title != null) {
+        editItem(item, title);
+      }
+    });
+  }
+
+  void editItem(Todo item, String title) {
+    item.title = title;
+  }
+
+  void _removeItemFromList(item) {
+    deleteItem(item);
+  }
+
+  void deleteItem(item) {
+    // We don't need to search for our item on the list because Dart objects
+    // are all uniquely identified by a hashcode. This means we just need to
+    // pass our object on the remove method of the list
+    items.remove(item);
+  }
+}
